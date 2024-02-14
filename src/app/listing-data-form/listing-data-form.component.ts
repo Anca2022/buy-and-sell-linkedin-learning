dfa1b0dd15dff21c2c2a0c85f74342fd1a131f8a
Anca Matei <ancamatei2012@gmail.com>
2024-02-14 22:16:36 +0200

    initial commit

---
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-data-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './listing-data-form.component.html',
+  styleUrl: './listing-data-form.component.css'
+})
+export class ListingDataFormComponent {
+
+}
